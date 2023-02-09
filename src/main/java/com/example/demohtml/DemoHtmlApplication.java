8e944a23e0a934484639d2eeebe5717aca9f7b27
danieljappe <danieljappe@icloud.com>
2023-02-09 14:42:27 +0100

    s

---
+++ src/main/java/com/example/demohtml/DemoHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHtmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohtml/DemoHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
